33da3c4e46c23eca1c107133a182483ed6b26363
makar <misha.torno@gmail.com>
2022-11-01 19:32:58 +0500

    поправил хендлеры, чтобы они не удалялись при нажатии "ctrl" + "o"

--- receiver/main.py
+++ receiver/main.py
@@ -1,7 +1,14 @@
+from handlers import start_command_handler, end_command_handler
 from receiver.models import bot
-from handlers import *
+
+
+def init_handlers():
+    bot.add_event_handler(start_command_handler)
+    bot.add_event_handler(end_command_handler)
+
 
 if __name__ == '__main__':
     bot.start()
+    init_handlers()
     print('bot start')
     bot.run_until_disconnected()
