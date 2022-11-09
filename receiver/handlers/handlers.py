292efda3c5c9a8929619212cd144bf33fc942fda
m.batuev <m.batuev@artsofte.ru>
2022-11-09 20:11:31 +0500

    добавил апи драйвер + почти закончил связь бота с бэком

--- receiver/handlers/handlers.py
+++ receiver/handlers/handlers.py
@@ -1,37 +1,35 @@
-from typing import List
-
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 
 from bot import BotState
-from utils import filter_helper
+from utils import filter_helper, file_helper
+from api_driver import api_driver
 
 
-async def start_handler(message: types.Message, state: FSMContext):
+async def start_handler(message: types.Message, state: FSMContext) -> None:
     await message.answer('Загрузите файлы', reply_markup=types.ReplyKeyboardRemove())
     await state.set_state(BotState.upload_files)
 
 
-async def end_handler(message: types.Message, state: FSMContext):
+async def end_handler(message: types.Message, state: FSMContext) -> None:
     await message.answer('Бот прекратил работу', reply_markup=types.ReplyKeyboardRemove())
     await state.finish()
 
 
-async def upload_files_handler(message: types.Message, files: List[types.Message]):
-    files_media_group = types.MediaGroup()
+async def upload_files_handler(message: types.Message, files: list[types.Message], state: FSMContext) -> None:
+    if not file_helper.is_files_type_correct(files):
+        await message.answer(f'Тип файлов некорректный\nПожалуйста загрузите файлы в одном из этих форматов {file_helper.allowed_file_types}',
+                             reply_markup=types.ReplyKeyboardRemove())
+        return
 
-    for file in files:
-        file_id = file[file.content_type].file_id
-        files_media_group.attach({'media': file_id, 'type': file.content_type})
-
-    await message.answer_media_group(files_media_group)
-
+    await file_helper.set_files_state(files, state)
     filter_buttons_markup = await filter_helper.get_filter_buttons_markup([])
+
     await message.answer('Выберите критерии подсчета дней:', reply_markup=filter_buttons_markup)
     await BotState.select_filters.set()
 
 
-async def select_filters_handler(message: types.Message, state: FSMContext):
+async def select_filters_handler(message: types.Message, state: FSMContext) -> None:
     if await filter_helper.can_change_state(state, message.text):
         await message.answer('Введите ссылку на Google Диск с итоговым файлом', reply_markup=types.ReplyKeyboardRemove())
         await BotState.send_google_url.set()
@@ -43,6 +41,11 @@ async def select_filters_handler(message: types.Message, state: FSMContext):
     await message.answer('Выберите критерии подсчета дней:', reply_markup=filter_buttons_markup)
 
 
-async def send_google_url_handler(message: types.Message, state: FSMContext):
-    await message.answer('итоговый урл')
+async def send_google_url_handler(message: types.Message, state: FSMContext) -> None:
+    data = await state.get_data()
+    url = message.values.get('text')
+    filters = data['filters']
+    files = data['files']
+    resp = await api_driver.write_data_by_url(files=files, filters=filters, google_doc_url=url)
+    await message.answer(resp)
     await state.finish()
